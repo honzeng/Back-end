e386f748d1681fcb3c5da09446558c3f92e376bd
Hon <honzeng@gmail.com>
2019-03-04 03:06:24 -0500

    Arreglo

--- src/main/java/co/com/ias/AvesAdmin/AvesAdmin/controllers/AdminAvesRestController.java
+++ src/main/java/co/com/ias/AvesAdmin/AvesAdmin/controllers/AdminAvesRestController.java
@@ -1,10 +1,13 @@
 package co.com.ias.AvesAdmin.AvesAdmin.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,18 +42,38 @@ public class AdminAvesRestController {
 	}
 	
 	@PutMapping("/listarAves/{cdave}")
-	public void actualizarAve(@RequestBody AvesModel registroave,@PathVariable String cdave) {		
-			Aves AvesE = avesrepository.registroByCdave(registroave.getCdave());
-			AvesE.setDsnombre_cientifico(registroave.getDsnombre_cientifico());
-			AvesE.setDsnombre_comun(registroave.getDsnombre_comun());
-			avesrepository.saveAndFlush(AvesE);
+	public ResponseEntity<Aves> actualizarAve(@RequestBody AvesModel registroave,@PathVariable String cdave) {		
 			
+	
+			Aves AvesE = avesrepository.registroByCdave(registroave.getCdave());
+			if (AvesE != null) {
+			Aves UptAve = AvesE;
+			UptAve.setDsnombre_cientifico(registroave.getDsnombre_cientifico());
+			UptAve.setDsnombre_comun(registroave.getDsnombre_comun());
+			return new ResponseEntity<>(avesrepository.save(UptAve), HttpStatus.OK);
+			}
+			else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
 	}
+		
 	
 	@DeleteMapping("/listarAves/{cdave}")
 	@Transactional
-	public void BorrarAves(@RequestBody Aves registrarAve) {
+	public ResponseEntity<String> borrarAve(@RequestBody Aves registrarAve,@PathVariable("cdave") String cdave) {
 		 avesrepository.delete(registrarAve);	
+		 return new ResponseEntity<>("La ave ha sido borrada!", HttpStatus.OK);
 	}
 	
+	
+	
+
+ 
+	@DeleteMapping("/listarAves/delete")
+	public ResponseEntity<String> borrarTodas() {
+ 
+		avesrepository.deleteAll();
+ 
+		return new ResponseEntity<>("Se borraron todas las aves!", HttpStatus.OK);
+	}
 	}
